8a12e813ead81d2e9f5291229d09c06272622d0e
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-05-27 23:47:02 +0100

    implemented carousel

--- src/components/sections/OurWorks/OurWorks.tsx
+++ src/components/sections/OurWorks/OurWorks.tsx
@@ -1,8 +1,18 @@
-import React from "react";
 import classes from "./OurWorks.module.css";
 import { Section, Container } from "../../../layout";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { OUR_WORKS } from "../../../helpers/constants";
 
 const OurWorks = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <Section id="our-works" background="white">
       <Container>
@@ -13,6 +23,16 @@ const OurWorks = () => {
             Here are some of the breath-taking projects we have constructed.
           </p>
         </div>
+        <Slider {...settings}>
+          {OUR_WORKS.map((item, index) => (
+            <img
+              key={index}
+              src={item.img_url}
+              alt="Our work"
+              className={classes.image}
+            />
+          ))}
+        </Slider>
       </Container>
     </Section>
   );
