a02d370f28c6c08ab925cb83f94c9b959f1bc719
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-05-30 00:03:05 +0100

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 import Footer from "./footer/Footer";
 import NavBar from "./navbar/NavBar";
-import Contact from "./contact/Contact";
 
-export { Footer, NavBar, Contact };
+export { Footer, NavBar };
